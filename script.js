341978520e1b62475bdf01cc8b12b5a9616921b2
VDI Student <ambica.varre@gmail.com>
2024-12-18 13:45:34 -0500

    portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+var   typed = new Typed(".loop",{
+    strings : [ "Full Stack Java Developer" , "AWS Certified Developer Associate" , "Terraform Certified Associate" ],
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : true
+})
